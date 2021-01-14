e6ccf526bbb397aea9e9cd1a427c913017d293c3
Alexander Slyotov <sletov@in4security.com>
2021-01-14 20:07:33 +0300

    sd368

--- lib/facter/nxlog_version.rb
+++ lib/facter/nxlog_version.rb
@@ -2,12 +2,10 @@ Facter.add(:nxlog_version) do
   setcode do
     if Facter.value('kernel') != 'windows'
       nxlog_version_command = '/opt/nxlog/bin/nxlog -h 2>&1 | head -n1'
-      nxlog_version = Facter::Util::Resolution.exec(nxlog_version_command)
-      %r{(nxlog)-([\w\.]+)}.match(nxlog_version)[2]
     else
       nxlog_version_command = 'c:\nxlog\nxlog.exe -h | findstr "^nxlog"'
-      nxlog_version = Facter::Util::Resolution.exec(nxlog_version_command)
-      %r{(nxlog)-([\w\.]+)}.match(nxlog_version)[2]
     end
+    nxlog_version = Facter::Util::Resolution.exec(nxlog_version_command)
+    %r{(nxlog)-([\w\.]+)}.match(nxlog_version)[2]
   end
 end
